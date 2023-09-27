b3e9222a7698a8976ab4c5f31e565198c1d27766
jinyuan2333 <jl14888@nyu.edu>
2023-09-27 09:00:12 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,31 @@
+let numCircles = 400;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  background(44, 62, 158);
+  fill(255, 150, 75);
+  stroke(255, 150, 75);
+  
+  randomSeed(90);
+
+  for (let i = 0; i < numCircles; i++) {
+    let x = random(width);
+    let y = random(height);
+    let dim = 15;
+    ellipse(x, y, dim, dim);
+  }
+
+  for (let i = 0; i < numCircles; i++) {
+    let x1 = random(width);
+    let y1 = random(height);
+    let x2 = random(width);
+    let y2 = random(height);
+
+    if (random(1) < 0.5) {
+      line(x1, y1, x2, y2);
+    }
+  }
 }
